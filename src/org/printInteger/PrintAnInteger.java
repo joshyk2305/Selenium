ae31364528338df04f61b5df80ef6140947a28df
joshyk2305 <joshyk2305@gmail.com>
2022-05-09 21:56:59 +0530

    Build is Ready

---
+++ src/org/printInteger/PrintAnInteger.java
@@ -0,0 +1,16 @@
+package org.printInteger;
+
+import java.util.Scanner;
+
+public class PrintAnInteger {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number");
+		
+		int no = sc.nextInt();
+		System.out.println("Entered Number is :"+no);
+	}
+
+}
